c0a339cc07bed3447a930636ff0074123bbc7d07
Rahunanthanan Thankavel <Rahunanthanan@Rahunanthanans-MacBook-Pro.local>
2016-10-13 13:43:28 +0530

    Initial commit

---
+++ samples/VuforiaSamples-6-0-120/app/build/generated/source/buildConfig/release/com/vuforia/samples/VuforiaSamples/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.vuforia.samples.VuforiaSamples;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.vuforia.samples.VuforiaSamples";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 600;
+  public static final String VERSION_NAME = "6.0";
+}
